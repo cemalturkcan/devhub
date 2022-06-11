11050dedb5236cb037b7b244ada99ce0ec5f0da3
Onur Demirkıran <onrdmrkrn1@gmail.com>
2022-06-11 16:05:11 +0300

    created
    created

---
+++ notificition/src/test/java/com/devhub/notificition/DevhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhub.notificition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
